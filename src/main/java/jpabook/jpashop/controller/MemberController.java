7387cf8a78fe51bd98b45f690dd129012f4ea7cf
Jinwon-Dev <yjw9424@naver.com>
2023-01-23 15:51:30 +0900

    회원 등록 로직 및 유효성 검증

---
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -0,0 +1,43 @@
+package jpabook.jpashop.controller;
+
+import jakarta.validation.Valid;
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.service.MemberService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MemberController {
+
+    private final MemberService memberService;
+
+    @GetMapping("/members/new")
+    public String createForm(Model model) {
+        model.addAttribute("memberForm", new MemberForm());
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(@Valid MemberForm form, BindingResult result) { // @Valid -> 유효성 검증 어노테이션을 이용해 검증해준다.
+        // MemberForm을 이용해서 필요한 형식에 맞게 데이터를 가공해서 보내자!
+
+        if (result.hasErrors()) {
+            return "members/createMemberForm";
+        }
+
+        Address address = new Address(form.getCity(), form.getStreet(), form.getZipcode());
+
+        Member member = new Member();
+        member.setName(form.getName());
+        member.setAddress(address);
+
+        memberService.join(member);
+        return "redirect:/"; // 첫 번째 페이지로 넘어감
+    }
+}
---
+++ src/main/java/jpabook/jpashop/controller/MemberForm.java
@@ -0,0 +1,16 @@
+package jpabook.jpashop.controller;
+
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter @Setter
+public class MemberForm {
+
+    @NotEmpty(message = "회원 이름은 필수 입니다")
+    private String name;
+
+    private String city;
+    private String street;
+    private String zipcode;
+}
