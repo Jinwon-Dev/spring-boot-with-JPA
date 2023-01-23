802a73d860159c2a7817710c249ce8b0a8e41e55
Jinwon-Dev <yjw9424@naver.com>
2023-01-23 16:10:52 +0900

    회원 목록 조회

--- src/main/java/jpabook/jpashop/controller/MemberController.java
+++ src/main/java/jpabook/jpashop/controller/MemberController.java
@@ -11,6 +11,8 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import java.util.List;
+
 @Controller
 @RequiredArgsConstructor
 public class MemberController {
@@ -39,5 +41,17 @@ public class MemberController {
 
         memberService.join(member);
         return "redirect:/"; // 첫 번째 페이지로 넘어감
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        /*
+            여기서도 엔티티를 직접 사용하는 것은 좋지 않다. 엔티티는 비즈니스 로직만 갖고 있고, 화면 종속적 기능은 갖고 있지 않는게 좋다.
+            유효성 점검 같은 경우도, 엔티티에서는 다른 유효성 점검이 필요할 수도 있고, 여러 차이가 의외로 많이 난다.
+            API를 만들 때는 꼭 Form 객체나 DTO를 사용하고, 절대 엔티티를 외부로 반환하지 말자!
+         */
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
     }
 }
