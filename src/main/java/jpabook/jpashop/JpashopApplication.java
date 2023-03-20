4a4f87e86f56d394c0082a2ef2ad352ed3bd21b2
Jinwon-Dev <yjw9424@naver.com>
2023-03-20 11:32:51 +0900

    install Hibernate5JakartaModule

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -1,7 +1,9 @@
 package jpabook.jpashop;
 
+import com.fasterxml.jackson.datatype.hibernate5.jakarta.Hibernate5JakartaModule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JpashopApplication {
@@ -10,4 +12,11 @@ public class JpashopApplication {
         SpringApplication.run(JpashopApplication.class, args);
     }
 
+    @Bean
+    Hibernate5JakartaModule hibernate5Module() {
+        Hibernate5JakartaModule hibernate5JakartaModule = new Hibernate5JakartaModule();
+        // 강제 지연 로딩 설정
+//        hibernate5JakartaModule.configure(Hibernate5JakartaModule.Feature.FORCE_LAZY_LOADING, true);
+        return hibernate5JakartaModule;
+    }
 }
